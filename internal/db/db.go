fd87a922407abf6e1d693c7aedf76a131099502f
SNinjo <sninjo657@gmail.com>
2025-08-09 09:54:23 +0800

    feat: turn off the GORM logging

--- internal/db/db.go
+++ internal/db/db.go
@@ -5,10 +5,13 @@ import (
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 func NewDatabase(config *config.Config) (*gorm.DB, error) {
-	db, err := gorm.Open(postgres.Open(config.DatabaseURL), &gorm.Config{})
+	db, err := gorm.Open(postgres.Open(config.DatabaseURL), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Silent),
+	})
 	if err != nil {
 		return nil, err
 	}
